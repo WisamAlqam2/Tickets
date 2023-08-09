58399153dcc294b54e5f9ee8f14434ef38e1723d
WisamAlqam2 <141641125+WisamAlqam2@users.noreply.github.com>
2023-08-09 16:11:38 +0300

    Add files via upload

---
+++ class12/Bussines Logic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class12
+{
+
+    
+    public class Food
+    {
+        public const double TAX = 2.00;
+
+        public string FoodName;
+        public double PriceFood;
+        public string Extra;
+    }
+
+    public class Menu
+    {
+        public List<Drink> Drinks { get; set; }
+        public List<Food> Foods { get; set; }
+    }
+
+    public class Drink
+    {
+        public const double TAX = 2.00;
+
+        public string DrinkName;
+        public double PriceDrink;
+
+        public string CheckVailedInput(string inputFromUser)
+        {
+            if (string.IsNullOrEmpty(inputFromUser))
+            {
+                throw new Exception("null");
+            }
+  
+            if(inputFromUser == "y")
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Ok, What Drink would you like?");
+                Console.WriteLine("Drink List");
+                return "y";
+                
+
+            }
+            else if (inputFromUser == "n")
+            {
+
+               
+                return "n ";
+            }
+            else
+            {
+                return "InvalidInput";
+            }
+        }
+
+    }
+    public class Restaurant
+    {
+        public double Amount;
+    }
+}
+
+
+
--- class12/Program.cs
+++ class12/Program.cs
@@ -11,40 +11,77 @@ namespace class12
 
             Console.WriteLine("Food List");
 
-            var foodList = new List<Food>
-            {
-                new Food { FoodName = "kitech",          PriceFood = 25 },
-                new Food { FoodName = "Fish",            PriceFood = 30 },
-                new Food { FoodName = "Burgar",          PriceFood = 20 },
-                new Food { FoodName = "kebab",           PriceFood = 25 },
-                new Food { FoodName = "Hummus and meat", PriceFood = 30 },
-                new Food { FoodName = "Prost",           PriceFood = 20 }
-            };
+            var foodList = GetDrinkList("F").Foods;
 
             for (int i = 0; i < foodList.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {foodList[i].FoodName} - {foodList[i].PriceFood}");
             }
 
-            Console.WriteLine("\n");
+            Console.WriteLine();
 
-            Console.Write("Choose the Drink you want: ");
+            Console.Write("Choose the Food you want: ");
             int foodChoice;
             while (!int.TryParse(Console.ReadLine(), out foodChoice) || foodChoice < 1 || foodChoice > foodList.Count)
             {
-                Console.Write("Please enter again: ");
+                Console.Write("Please enter a valid choice: ");
             }
 
             Food selectedFood = foodList[foodChoice - 1];
+            var drink = new Drink();
 
-            Console.Write("Would you like a drink with that? (y/n): ");
+            Console.Write("Would you like a drink with that? (y/n):\n ");
             string userInput = Console.ReadLine();
 
-            if (userInput.ToLower() == "y")
+            var fromDrink = drink.CheckVailedInput(userInput);
+            while (fromDrink == "InvalidInput")
             {
-                Console.WriteLine(" ok , what drink would you like?");
-                Console.WriteLine("Drink List");
+                Console.WriteLine("Invalid input. Please enter 'y' or 'n'");
+                Console.Write("Would you like a drink with that? (y/n): ");
+                userInput = Console.ReadLine();
+                fromDrink = drink.CheckVailedInput(userInput);
+            }
 
+            if (userInput == "y")
+            {
+
+                List<Drink> drinkList = GetDrinkList("D").Drinks;
+                for (int i = 0; i < drinkList.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {drinkList[i].DrinkName} - {drinkList[i].PriceDrink}");
+                }
+                int drinkChoice;
+
+                Console.Write("\nChoose the Drink you want: ");
+                while (!int.TryParse(Console.ReadLine(), out drinkChoice) || drinkChoice < 1 || drinkChoice > drinkList.Count)
+                {
+                    Console.Write("Please enter a valid choice: ");
+                }
+                
+                Drink selectedDrink = drinkList[drinkChoice - 1];
+                Console.WriteLine($" {selectedFood.FoodName} - {selectedFood.PriceFood} and {selectedDrink.DrinkName} - {selectedDrink.PriceDrink}");
+
+
+                var Amount = selectedFood.PriceFood + selectedDrink.PriceDrink;
+
+                Console.Write("Amount :");
+                Console.Write(Amount);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Thank You");
+            Console.ReadKey();
+
+            Console.WriteLine($" {selectedFood.PriceFood}");
+
+        }
+
+        private static Menu GetDrinkList(string type)
+        {
+            var menu = new Menu();
+
+            if (type == "D")
+            {
                 var drinkList = new List<Drink>
                 {
                     new Drink { DrinkName = "Coffee",           PriceDrink = 25 },
@@ -54,35 +91,41 @@ namespace class12
                     new Drink { DrinkName = "Cola With Ice",    PriceDrink = 30 },
                     new Drink { DrinkName = "Lemon",            PriceDrink = 20 },
                     new Drink { DrinkName = "Mint lemon",       PriceDrink = 20 }
+
                 };
 
-                for (int i = 0; i < drinkList.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {drinkList[i].DrinkName} - {drinkList[i].PriceDrink}");
-                }
+                menu.Drinks = drinkList;
 
-                Console.WriteLine("\n");
+                return menu;
 
-                Console.Write("Choose the Drink you want: ");
-                int drinkChoice;
-                while (!int.TryParse(Console.ReadLine(), out drinkChoice) || drinkChoice < 1 || drinkChoice > drinkList.Count)
-                {
-                    Console.Write("Please enter again: ");
-                }
 
-                Drink selectedDrink = drinkList[drinkChoice - 1];
-                Console.WriteLine($" {selectedFood.FoodName} - {selectedFood.PriceFood} and {selectedDrink.DrinkName} - {selectedDrink.PriceDrink}");
-                Console.WriteLine($" {selectedFood.PriceFood + selectedDrink.PriceDrink}");
             }
-            else
+
+            if (type == "F")
             {
-                Console.WriteLine($" {selectedFood.FoodName} - {selectedFood.PriceFood}");
-                Console.WriteLine($" {selectedFood.PriceFood}");
-            }
+                var foodList = new List<Food>
+                 {
+                new Food { FoodName = "kitech",          PriceFood = 25 },
+                new Food { FoodName = "Fish",            PriceFood = 30 },
+                new Food { FoodName = "Burgar",          PriceFood = 20 },
+                new Food { FoodName = "kebab",           PriceFood = 25 },
+                new Food { FoodName = "Hummus and meat", PriceFood = 30 },
+                new Food { FoodName = "Prost",           PriceFood = 20 }
+                 };
 
-            Console.WriteLine("\n");
-            Console.WriteLine("Thank You");
-            Console.ReadKey();
+                menu.Foods = foodList;
+
+                return menu;
+            }
+            return null;
         }
+
+
     }
+
+
 }
+
+
+
+
